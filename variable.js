79cdeada3bf17ef443dba782a87905c3ecdf143b
Jatin Nayak <jatinnayak0947@gmail.com>
2024-09-09 20:06:32 +0530

    till video 2

---
+++ variable.js
@@ -0,0 +1,45 @@
+// // video --1
+// const accountId = 144553
+// let accountEmail = "jatinnayak0909@gmail.com"
+// var accountPasswords = "1234"
+// accountcity = "Bubaneswar"
+
+// // accountId = 2; // if const is used before a variavle name it cannot be modified .
+// console.log(accountId);
+// accountEmail = "jatin@gmail.com"
+// accountPasswords = "123"
+// accountcity = "DSS"
+
+// console.table([accountId , accountEmail ,accountPasswords , accountcity])
+
+// //there are two different types of method to declear variables 
+// //1. let
+// //2. var -> not used these day because of its no restriction in scope of variable
+// // prefer not to use var because issues of sblock and functional scope
+
+// // if the value is not assigned to the variable at the time of decleration it remains as undefinded
+
+
+// // video -- 2
+
+
+// // "use strict" treat all JS code as newer version
+// //alert(3+3) it will not work as we are not working on browser we are on node js
+
+
+//primitive data types
+/*
+number = 2^53
+bigint
+string
+boolean
+null  -> standalone value
+undefined
+symbol -> to find and assign the uniqueness
+
+*/
+
+// to check the data type
+//console.log(typeof data);
+// undefined shows data tyoes as undefined ,  which is different from the normal behaviour of the data type
+// null shows data types as object , which is different from the normal behaviour of the data type
