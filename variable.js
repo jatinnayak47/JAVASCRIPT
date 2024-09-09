76de50661a3c4c34eca2ff930ef4589f35690d0d
Jatin Nayak <jatinnayak0947@gmail.com>
2024-09-09 21:23:17 +0530

    video number 4

--- variable.js
+++ variable.js
@@ -1,57 +1,76 @@
-// // video --1
-// const accountId = 144553
-// let accountEmail = "jatinnayak0909@gmail.com"
-// var accountPasswords = "1234"
-// accountcity = "Bubaneswar"
+// // // video --1
+// // const accountId = 144553
+// // let accountEmail = "jatinnayak0909@gmail.com"
+// // var accountPasswords = "1234"
+// // accountcity = "Bubaneswar"
 
-// // accountId = 2; // if const is used before a variavle name it cannot be modified .
-// console.log(accountId);
-// accountEmail = "jatin@gmail.com"
-// accountPasswords = "123"
-// accountcity = "DSS"
+// // // accountId = 2; // if const is used before a variavle name it cannot be modified .
+// // console.log(accountId);
+// // accountEmail = "jatin@gmail.com"
+// // accountPasswords = "123"
+// // accountcity = "DSS"
 
-// console.table([accountId , accountEmail ,accountPasswords , accountcity])
+// // console.table([accountId , accountEmail ,accountPasswords , accountcity])
 
-// //there are two different types of method to declear variables 
-// //1. let
-// //2. var -> not used these day because of its no restriction in scope of variable
-// // prefer not to use var because issues of sblock and functional scope
+// // //there are two different types of method to declear variables 
+// // //1. let
+// // //2. var -> not used these day because of its no restriction in scope of variable
+// // // prefer not to use var because issues of sblock and functional scope
 
-// // if the value is not assigned to the variable at the time of decleration it remains as undefinded
-
-
-// // video -- 2
+// // // if the value is not assigned to the variable at the time of decleration it remains as undefinded
 
 
-// // "use strict" treat all JS code as newer version
-// //alert(3+3) it will not work as we are not working on browser we are on node js
+// // // video -- 2
 
 
-//primitive data types
+// // // "use strict" treat all JS code as newer version
+// // //alert(3+3) it will not work as we are not working on browser we are on node js
+
+
+// //primitive data types
+// /*
+// number = 2^53
+// bigint
+// string
+// boolean
+// null  -> standalone value
+// undefined
+// symbol -> to find and assign the uniqueness
+
+// */
+
+// // to check the data type
+// //console.log(typeof data);
+// // undefined shows data tyoes as undefined ,  which is different from the normal behaviour of the data type
+// // null shows data types as object , which is different from the normal behaviour of the data type
+
+
+// //video -> 3
+// //type casting 
+// // datatype(data) _-> first letter should be in caps 
+
+// /* if the input is  like 232cs in the number data type , 
+// int this case if we check the data type of this inputed value it will
+//  show its datatypes as number  but if we try to kprint the value of 
+//  that variable next time it will show output as NaN(not a number)*/
+
+//  // video 4
+//  /*
+//   operations in js
+//     use  - symbole before variable name to make it negative module
+     
+//     according to the rule 
+//     this operation and there given output 
+
+//     ("1" + 2 + 2) -> its output will be 122
+
+//     (1 + 2 + "2") -> its output will be 132
+        
 /*
-number = 2^53
-bigint
-string
-boolean
-null  -> standalone value
-undefined
-symbol -> to find and assign the uniqueness
+        console.log(true)
+        if we change this line to console.loog(+true) the output will
+        be 1 but console.log(true+) this will throw an error of
+        unexpected at this place
 
 */
 
-// to check the data type
-//console.log(typeof data);
-// undefined shows data tyoes as undefined ,  which is different from the normal behaviour of the data type
-// null shows data types as object , which is different from the normal behaviour of the data type
-
-
-//video -> 3
-//type casting 
-// datatype(data) _-> first letter should be in caps 
-
-/* if the input is  like 232cs in the number data type , 
-int this case if we check the data type of this inputed value it will
- show its datatypes as number  but if we try to kprint the value of 
- that variable next time it will show output as NaN(not a number)*/
-
- 
